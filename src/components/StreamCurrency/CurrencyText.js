4f7b8328a465abf149f2398382464439da61a1ae
evenar <bugracelebi55@hotmail.com>
2023-06-20 03:43:10 +0300

    add theme

--- src/components/StreamCurrency/CurrencyText.js
+++ src/components/StreamCurrency/CurrencyText.js
@@ -1,8 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {colors} from '../DarkMode/colors';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 const CurrencyText = ({title, rates, onAddToWatchlist}) => {
+
+    const {theme} = useContext(ThemeContext);
+	let activeColors = colors[theme.mode];
 
     const [showOnlyFavorites, setShowOnlyFavorites] = useState(false);
     const [favorite, setFavorite] = useState();
@@ -12,9 +17,9 @@ const CurrencyText = ({title, rates, onAddToWatchlist}) => {
     };
 
   return (
-    <View style={styles.item}>
-        <Text style={styles.name}>{title}</Text>
-        <Text style={styles.name}>{rates}</Text>
+    <View style={[styles.item, {backgroundColor: activeColors.secondary}]}>
+        <Text style={[styles.name, {color: activeColors.tint}]}>{title}</Text>
+        <Text style={[styles.name, {color: activeColors.tint}]}>{rates}</Text>
         <Ionicons iconName="star-outline" name={favorite ? "star-sharp" :  "star-outline"} onPress={() => setFavorite(!favorite)}  size={50} color='#666' style={{marginRight: 5}} />
     </View>
   )
@@ -24,13 +29,11 @@ export default CurrencyText;
 
 const styles = StyleSheet.create({
     item: {
-        backgroundColor: '#f6f6f6',
         padding: 20,
         marginVertical: 8,
         marginHorizontal: 16,
         borderRadius: 10,
         borderWidth: 2,
-        borderColor: '#ccc',
         flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
