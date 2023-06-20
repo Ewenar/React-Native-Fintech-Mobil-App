da882bb23380a95ca9f5ae1eca1110c6c0ca8344
evenar <bugracelebi55@hotmail.com>
2023-06-20 23:43:38 +0300

    title translate

--- src/components/CreditCard/SavedCard/SavedHeader.js
+++ src/components/CreditCard/SavedCard/SavedHeader.js
@@ -3,8 +3,12 @@ import React, {useState, useContext} from 'react';
 import { ThemeContext } from '../../contexts/ThemeContext';
 import {colors} from '../../DarkMode/colors';
 import LogoutPower from '../../Login/LogoutPower';
+import '../../../../languages/i18n';
+import { useTranslation } from 'react-i18next';
 
 export default function SavedHeader({pressGear}) {
+
+  const {t, i18n} = useTranslation();
 
   const {theme} = useContext(ThemeContext);
   let activeColors = colors[theme.mode];
@@ -13,7 +17,7 @@ export default function SavedHeader({pressGear}) {
   return (
     <SafeAreaView>
         <View style={styles.container}>
-          <Text style={[styles.title, {color: activeColors.tertiary}]}>Saved Cards</Text>
+          <Text style={[styles.title, {color: activeColors.tertiary}]}>{t('savedList.SavedCardTitle')}</Text>
         </View>
     </SafeAreaView>
   )
